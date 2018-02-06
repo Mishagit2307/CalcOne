0404e2d8a6179ed965e00b4722295b79ed4105a6
Mishagit2307 <bolzano1972@mail.ru>
2018-02-06 06:32:29 +0200

    Дописав коменти

--- app/src/main/java/info/misha/calcone/MainActivity.java
+++ app/src/main/java/info/misha/calcone/MainActivity.java
@@ -10,22 +10,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView resultField;
-    EditText numberField;
-    TextView operationField;
-    Double operand = null;
-    String lastOperation = "=";
+    TextView resultField; //текстове поле для виводу результата
+    EditText numberField; //поле для введення числа
+    TextView operationField; //поле вивід знака операцїї
+    Double operand = null; //операнд
+    String lastOperation = "="; //остання операція
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        resultField = findViewById(R.id.resultField);
+        resultField = findViewById(R.id.resultField); //отримуємо поля по id з activity_main.xml
         numberField =  findViewById(R.id.numberField);
         operationField =  findViewById(R.id.operationField);
     }
-
+            //збереження стану
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putString("OPERATION", lastOperation);
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
             outState.putDouble("OPERAND", operand);
         super.onSaveInstanceState(outState);
     }
-
+          //отримання збереженного стану
     @SuppressLint("SetTextI18n")
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         resultField.setText(operand.toString());
         operationField.setText(lastOperation);
     }
-
+         //обробка натискання на числову кнопку
     public void onNumberClick(View view){
 
         Button button = (Button)view;
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
             operand = null;
         }
     }
-
+          //обробка натискання на кнопку стерти
     public void onClearClick(View view) {
         this.lastOperation = "";
         this.operand = null;
@@ -66,13 +66,13 @@ public class MainActivity extends AppCompatActivity {
         numberField.setText("");
         operationField.setText("");
     }
-
+          //обробка натискання на кнопку операцїї
     public void onOperationClick(View view){
 
         Button button = (Button)view;
         String op = button.getText().toString();
         String number = numberField.getText().toString();
-
+          //якщо введено не числа
         if(number.length()>0){
             number = number.replace(',', '.');
             try{
@@ -88,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("SetTextI18n")
     private void performOperation(Double number, String operation){
         boolean divideByZero = false;
-
+         // якщо операнд раніше не був встановлений
         if(operand ==null){
             operand = number;
         }
@@ -120,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
                     break;
             }
         }
-
+          // перевірка ділення на ноль
         if (divideByZero) {
             resultField.setText("ERROR: Divide by zero");
         } else {
